40ad865f472adf22afceb83ce398b9a0447232cd
Grigoash Pasha <GrigorashPasha@gmail.com>
2021-11-15 08:46:26 +0300

    add post by tags

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -4,5 +4,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', Home.as_view(), name='home'),
+    path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
+    path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
+    path('post/<str:slug>/', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
 ]
+
